58c14ba09caafb747f4c68e3a9e7df7bcbce7262
sdegutis <sbdegutis@gmail.com>
2020-05-29 10:41:38 -0500

    Got input balances working

--- main.js
+++ main.js
@@ -3,27 +3,46 @@ const balanceToPayEl = /**@type HTMLInputElement*/(document.getElementById('bala
 const balanceDueEl = /**@type HTMLInputElement*/(document.getElementById('balanceDue'));
 
 const balances = {
-  amount: 0,
-  toPay: 0,
-  due: 0,
+  amount: 10,
+  toPay: 20,
+  due: 30,
 };
 
-balanceAmountEl.onkeydown = changeBalance(balanceAmountEl, 'amount');
-balanceToPayEl.onkeydown = changeBalance(balanceToPayEl, 'toPay');
-balanceDueEl.onkeydown = changeBalance(balanceDueEl, 'due');
+balanceAmountEl.value = formatMoney(balances.amount);
+balanceToPayEl.value = formatMoney(balances.toPay);
+balanceDueEl.value = formatMoney(balances.due);
+
+setupChangeBalance(balanceAmountEl, 'amount');
+setupChangeBalance(balanceToPayEl, 'toPay');
+setupChangeBalance(balanceDueEl, 'due');
 
 /**
  * @param {HTMLInputElement} el
  * @param {keyof balances} key
  */
-function changeBalance(el, key) {
-  /** @type {(this: GlobalEventHandlers, ev: KeyboardEvent) => any} */
-  return function handler(e) {
-    if (e.keyCode === 13) {
+function setupChangeBalance(el, key) {
+  function cancel() {
+    el.value = formatMoney(balances[key]);
+    el.blur();
+  }
 
+  el.onblur = (e) => cancel();
+  el.onkeydown = (e) => {
+    if (e.keyCode === 13) {
+      balances[key] = parseMoney(el.value);
+      el.value = formatMoney(balances[key]);
+      el.blur();
     }
     else if (e.keyCode === 27) {
-
+      cancel();
     }
   };
+}
+
+function formatMoney(/** @type {number} */ amount) {
+  return amount.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+}
+
+function parseMoney(/** @type {string} */ amount) {
+  return parseFloat(amount.replace(/\$/g, ''));
 }
