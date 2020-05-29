724e337d2a0a02b94c552d47f8e1b787534cdb15
sdegutis <sbdegutis@gmail.com>
2020-05-29 09:50:42 -0500

    Menu

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { app, BrowserWindow } = require('electron');
+const electron = require('electron');
 const path = require('path');
 
-app.whenReady().then(() => {
-  const mainWindow = new BrowserWindow({
+electron.app.whenReady().then(() => {
+  const mainWindow = new electron.BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -12,5 +12,40 @@ app.whenReady().then(() => {
 
   mainWindow.loadFile('index.html');
 
-  mainWindow.webContents.openDevTools();
+  const newFile = () => { };
+  const openFile = () => { };
+  const saveFile = () => { };
+  const saveAsFile = () => { };
+  const exit = () => { };
+  const undo = () => { };
+  const redo = () => { };
+  const addExpense = () => { };
+  const addSpace = () => { };
+
+  mainWindow.on('close', (e) => {
+    //e.preventDefault();
+  });
+
+  mainWindow.setMenu(electron.Menu.buildFromTemplate([
+    {
+      label: '&File', submenu: [
+        { label: '&New', accelerator: 'Ctrl+N', click: newFile },
+        { label: '&Open', accelerator: 'Ctrl+O', click: openFile },
+        { type: 'separator' },
+        { label: '&Save', accelerator: 'Ctrl+S', click: saveFile },
+        { label: 'Save &As', click: saveAsFile },
+        { type: 'separator' },
+        { label: 'E&xit', click: exit },
+      ],
+    },
+    {
+      label: '&Edit', submenu: [
+        { label: '&Undo', accelerator: 'Ctrl+Z', click: undo },
+        { label: '&Redo', accelerator: 'Ctrl+Y', click: redo },
+        { type: 'separator' },
+        { label: 'Add Expense', click: addExpense },
+        { label: 'Add Space', click: addSpace },
+      ],
+    },
+  ]));
 });
